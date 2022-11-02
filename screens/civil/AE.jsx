6731bcabc9f98e505dc6b4c94e8b26704dd04d06
RafsanTanveer <rzrt.edu@gmail.com>
2022-11-02 15:14:33 +0600

    fromOffice02-11-2022

--- screens/civil/AE.jsx
+++ screens/civil/AE.jsx
@@ -1,81 +1,92 @@
 import React, { useState } from "react";
-import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Button, FlatList, Image, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-
-
-// const data = [
-//     {
-//         id: '1',
-//         name: 'sdf',
-//         designation: 'sdfasd',
-//         office: 'asdfsd',
-//         contactNumber: '564654',
-//         officeNumber: '444'
-//     },
-//     {
-//         id: '2',
-//         name: 'sdf',
-//         designation: 'sdfasd',
-//         office: 'asdfsd',
-//         contactNumber: '564654',
-//         officeNumber: '444456'
-//     },
-//     {
-//         id: '1',
-//         name: 'sxcvxcvdf',
-//         designation: 'sdfafghfsd',
-//         office: 'asdfdfgdfsd',
-//         contactNumber: '564ghjghj654',
-//         officeNumber: '4sdfdf44'
-//     },
-//     {
-//         id: '3',
-//         name: 'ssdfxcvbsddf',
-//         designation: 'ssdfdfasd',
-//         office: 'asdsdfsdffsd',
-//         contactNumber: '564654',
-//         officeNumber: '445644'
-//     },
-
-    
-//     {
-//         id: '4',
-//         name: 'sdsdfgdfsdff',
-//         designation: 'sdfasvbmnd',
-//         office: 'asdfsdvbxcvb',
-//         contactNumber: '56sdf4654',
-//         officeNumber: '456444'
-//     },
-//     {
-//         id: '5',
-//         name: 'sdfsdfdghf',
-//         designation: 'sdfacvbsd',
-//         office: 'asdcvbfsd',
-//         contactNumber: '56sdf4654',
-//         officeNumber: '444564'
-//     }
-// ]
 
 const DATA = [
     {
-        id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-        title: "First Item",
+        id: '920209001',
+        name: 'Rafsan Zani Rabbi',
+        designation: 'Assistant Programmer',
+        image: 'https://0.academia-photos.com/20936688/5794344/6584685/s200_rafsan.tanveer.jpg',
+        office: 'Directorate of Human Resource Development',
+        personalContact: '01795045357',
+        officeContact: '30309'
     },
     {
-        id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-        title: "Second Item",
+        id: '9202049001',
+        name: 'Rafsan Zani Rabbi',
+        designation: 'Assistant Programmer',
+        image: 'https://0.academia-photos.com/20936688/5794344/6584685/s200_rafsan.tanveer.jpg',
+        office: 'Directorate of Human Resource Development',
+        personalContact: '01795045357',
+        officeContact: '30309'
     },
     {
-        id: "58694a0f-3da1-471f-bd96-145571e29d72",
-        title: "Third Item",
+        id: '9202079001',
+        name: 'Rafsan Zani Rabbi',
+        designation: 'Assistant Programmer',
+        image: 'https://0.academia-photos.com/20936688/5794344/6584685/s200_rafsan.tanveer.jpg',
+        office: 'Directorate of Human Resource Development',
+        personalContact: '01795045357',
+        officeContact: '30309'
+    },
+    {
+        id: '9201209001',
+        name: 'Rafsan Zani Rabbi',
+        designation: 'Assistant Programmer',
+        image: 'https://0.academia-photos.com/20936688/5794344/6584685/s200_rafsan.tanveer.jpg',
+        office: 'Directorate of Human Resource Development',
+        personalContact: '01795045357',
+        officeContact: '30309'
+    },
+    {
+        id: '9202099001',
+        name: 'Rafsan Zani Rabbi',
+        designation: 'Assistant Programmer',
+        image: 'https://0.academia-photos.com/20936688/5794344/6584685/s200_rafsan.tanveer.jpg',
+        office: 'Directorate of Human Resource Development',
+        personalContact: '01795045357',
+        officeContact: '30309'
     },
 ];
 
-
 const Item = ({ item, onPress, backgroundColor, textColor }) => (
-    <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
-        <Text style={[styles.title, textColor]}>{item.title}</Text>
-    </TouchableOpacity>
+
+
+    <View style={{ flexDirection: 'row', paddingLeft: 10, paddingRight: 10, height: 190 }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
+            <Image style={styles.logo} source={{
+                uri: item.image
+            }} />
+        </View>
+        <View style={{
+            flex: 3, borderBottomColor: 'grey',
+            borderBottomWidth: StyleSheet.hairlineWidth, padding: 5
+        }}>
+            <Text style={{ fontSize: 17, fontFamily: 'serif', fontWeight: 'bold' }} >{item.name} </Text>
+            <Text style={{ fontSize: 15, fontFamily: 'serif', marginTop: 3, color: 'grey' }}>{item.designation} </Text>
+            <Text style={{ fontFamily: 'serif', marginTop: 3, color: 'grey' }}>{item.office} </Text>
+
+            <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                <Text style={{ flex: 1.2, justifyContent: 'center', alignContent: 'center' }} >Personal Contact</Text>
+                <Text style={{ flex: 1 }}>{item.personalContact}</Text>
+                <Button style={{ flex: 1 }} title="Call" />
+            </View>
+            <View style={{ flexDirection: 'row', alignContent: 'center', marginTop: 10 }}>
+                <Text style={{ flex: 1.2, justifyContent: 'center' }} >Office</Text>
+                <Text style={{ flex: 1 }}>{item.officeContact}</Text>
+
+                <TouchableOpacity onPress={onPress} style={styles.button}>
+                    <Text style={styles.buttonText}>Call</Text>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    </View>
+
+    // <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+    //     <Text style={[styles.title, textColor]}>{item.title}</Text>
+    // </TouchableOpacity>
 );
 
 
@@ -100,6 +111,7 @@ const AE = ({ navigation }) => {
     return (
         <SafeAreaView style={styles.container}>
             <FlatList
+
                 data={DATA}
                 renderItem={renderItem}
                 keyExtractor={(item) => item.id}
@@ -108,7 +120,6 @@ const AE = ({ navigation }) => {
         </SafeAreaView>
     )
 }
-
 
 const styles = StyleSheet.create({
     container: {
@@ -123,10 +134,25 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 32,
     },
+    logo: {
+        width: 86,
+        height: 88,
+
+    },
+    button: {
+        backgroundColor: "gray",
+        height: 40,
+        width:60,
+        //padding: 10,
+        borderRadius: 10
+    },
+    buttonText: {
+        color: "white",
+        alignContent: 'center',
+        justifyContent:'center'
+    }
 });
 
-
 export default AE
-
 
 
